1a6bb9b1b939de57602a6faeda4f6a18e1bbae8a
skandregula <Satyendra_Kandregula@epam.com>
2016-11-21 11:56:52 +0530

    Removed Old Code Of SNL

--- SimpleTaskSystem.Web/App/Main/views/holders/holders.js
+++
@@ -1,9 +0,0 @@
-﻿(function () {
-    var controllerId = 'app.views.holders';
-    angular.module('app').controller(controllerId, [
-        '$scope', function ($scope) {
-            var vm = this;
-            //About logic...
-        }
-    ]);
-})();
