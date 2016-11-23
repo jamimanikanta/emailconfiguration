0288d9d4b32a6fe841c1a24750292ccfffdc4fb0
Manasa Karri <Pavan_Gautaraju@epam.com>
2016-11-23 19:53:08 +0530

    Added Test cases for Web API

--- Smps.WebApi.Tests/UserAccountTest.cs
+++ Smps.WebApi.Tests/UserAccountTest.cs
@@ -1,36 +1,51 @@
 ﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Castle.Windsor;
+using System.Linq;
 using Castle.MicroKernel.Registration;
+using Castle.Windsor;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Smps.Core.Interfaces.Account;
+using Smps.DAL;
 using Smps.WebApi.Controllers;
 
 namespace Smps.WebApi.Tests.UserAccountTest
 {
+    /// <summary>
+    /// Test class for UserAccountController
+    /// </summary>
     [TestClass]
     public class UserAccountTest : IDisposable
     {
+        private bool disposedValue = false;
         WindsorContainer container;
+
+        /// <summary>
+        /// Register the Assemblies
+        /// </summary>
         public UserAccountTest()
         {
             container = new WindsorContainer();
             container.Register(Classes.FromAssemblyNamed("Smps.Dal").Where(type => type.IsPublic).WithService.DefaultInterfaces().LifestyleTransient());
             container.Register(Classes.FromAssemblyNamed("Smps.core").Where(type => type.IsPublic).WithService.DefaultInterfaces().LifestyleTransient());
         }
+        /// <summary>
+        /// Method to test whether User is valid or not
+        /// </summary>
         [TestMethod]
         public void IsUserValid()
         {
-            string userId = "1";
-            string password = "test";
+            User user;
+            using (SMPSEntities objectContext = new SMPSEntities())
+            {
+                IQueryable<User> users = objectContext.Users;
+                user = users.FirstOrDefault();
+            }
             IUserAccount obj = container.Resolve<IUserAccount>();
-            UserAccountController user = new UserAccountController(obj);
-            bool returnval = user.IsUserValid(userId, password);
-            Assert.AreEqual(false, returnval);
+            UserAccountController userObj = new UserAccountController(obj);
+            bool returnval = userObj.IsUserValid(user.UserLoginId, user.UserLoginPassword);
+            Assert.AreEqual(true, returnval);
         }
 
-        #region IDisposable Support
-        private bool disposedValue = false; // To detect redundant calls
-
+        #region IDisposable Support        
         protected virtual void Dispose(bool disposing)
         {
             if (!disposedValue)
@@ -38,29 +53,13 @@ namespace Smps.WebApi.Tests.UserAccountTest
                 if (disposing)
                 {
                     this.container.Dispose();
-                    // TODO: dispose managed state (managed objects).
                 }
-
-                // TODO: free unmanaged resources (unmanaged objects) and override a finalizer below.
-                // TODO: set large fields to null.
-
                 disposedValue = true;
             }
         }
-
-        // TODO: override a finalizer only if Dispose(bool disposing) above has code to free unmanaged resources.
-        // ~Account() {
-        //   // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
-        //   Dispose(false);
-        // }
-
-        // This code added to correctly implement the disposable pattern.
         public void Dispose()
-        {
-            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+        {            
             Dispose(true);
-            // TODO: uncomment the following line if the finalizer is overridden above.
-            // GC.SuppressFinalize(this);
         }
         #endregion
     }
