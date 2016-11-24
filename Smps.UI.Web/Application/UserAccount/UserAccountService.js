e1ea7d07166a7aa6665502a0a394151a1936a8d3
Venkatesh Pydi <Venkatesh_Pydi@epam.com>
2016-11-24 22:21:23 +0530

    Resolving conflicts due to merge issues with others

--- Smps.UI.Web/Application/UserAccount/UserAccountService.js
+++ Smps.UI.Web/Application/UserAccount/UserAccountService.js
@@ -1,18 +1,28 @@
-﻿(function () {
-    angular.module('SMPSapp').factory('userAccountService', ['$http', '$rootScope', function ($http, $rootScope) {
+﻿//-----------------------------------------------------------------------
+// <copyright file="UserAccountService" company="EPAM">
+//     EPAM copyright .
+// </copyright>
+//<summary>This is the User account Service.</summary>
+// should be able to communicate eith web api for crud opration.
+//-----------------------------------------------------------------------
+(function () {
+    angular.module('SMPSapp').factory('userAccountService', ['$http', '$rootScope', userAccountService])
 
-        //// Used Closers to absracting the method
+    /* To validate the entred user emailid and password   */
+    /* This method calls service to validate the given credntials */
+    function userAccountService($http, $rootScope) {
+        var userProfile;
         return {
+            userProfile: userProfile,
             authenticateUser: function (userObject) {
                 return $http(
                          {
                              method: 'GET',
-                             url: $rootScope.apiURL + "UserAccount/IsUserValid?userId="
-                                     + userObject.userName + "&password="
+                             url: $rootScope.apiURL + 'UserAccount/ValidateUser?userId='
+                                     + userObject.userName + '&password='
                                      + userObject.password
                          });
             }
-        }
-
-    }]);
+        };
+    };
 })();
--- Smps.UI.Web/Application/UserAccount/loginCtrl.js
+++ Smps.UI.Web/Application/UserAccount/loginCtrl.js
@@ -1,28 +1,40 @@
-﻿(function () {
+﻿//-----------------------------------------------------------------------
+// <copyright file="Login Controller" company="EPAM">
+//     EPAM copyright @2016.
+// </copyright>
+//<summary>This is login controller.
+//As a User, Login page with organization logo 
+//should be available to access EPAM SMPS application
+//</summary>
+//-----------------------------------------------------------------------
+(function () {
     angular.module('SMPSapp').controller("loginCtrl", ['$scope', '$rootScope', 'userAccountService', '$state', function ($scope, $rootScope, userAccountService, $state) {
-        $scope.userObject = { userName: "", password: "" };
-        $scope.message = "";
+        $scope.userObject = { userName: '', password: '' };
+        $scope.message = '';
+
+        /* To validate the entred user emailid and password   */
+        /* This method calls service to validate the given credntials */
         $scope.login = function () {
             $scope.errorFlag = false;
             $scope.userObject;
 
-            ////To call Service
+
+            /* This method calls service to validate the given credntials */
             userAccountService.authenticateUser($scope.userObject)
             .then(
                     function (response) {
-                        if (response.data == true) {
-                            $scope.message = "";
+                        if (response.data && response.data !== 'null' && response.data !== 'undefined') {
+                            $scope.message = ''
+                            userAccountService.userProfile = response.data;
                             $state.go('home');
                         }
                         else {
-                            $scope.message = "Incorrect email id or password entered. Please try again";
+                            $scope.message = 'Incorrect email id or password entered. Please try again';
                         }
 
-                    }).catch(function (response, status) {
-                        $scope.message = "Login Failed.Due to server not responding";
+                    }).catch(function () {
+                        $scope.message = 'Login Failed: Invalid user';
                     });
         };
-
-
     }]);
 })();
