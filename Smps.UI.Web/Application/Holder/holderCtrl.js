cc11b592913870db411562d814774fe0a108dcc3
Venkatesh Pydi <Venkatesh_Pydi@epam.com>
2016-11-24 22:14:44 +0530

    Resolving conflicts due to merge issues

--- Smps.UI.Web/Application/Holder/holderCtrl.js
+++ Smps.UI.Web/Application/Holder/holderCtrl.js
@@ -1,23 +1,37 @@
-﻿(function(){
-    angular.module("SMPSapp")
-           .controller("HolderCtrl", ['$scope', '$rootScope', '$http', HolderCtrl]);
+﻿//-----------------------------------------------------------------------
+// <copyright file="holder Controller" company="EPAM">
+//     EPAM copyright @2016.
+// </copyright>
+//<summary>This is holder controller.</summary>
+//-----------------------------------------------------------------------
+(function () {
+    angular.module('SMPSapp')
+               .controller('holderCtrl', ['$scope', '$rootScope', '$http', 'userAccountService', holderCtrl]);
 
-    function HolderCtrl($scope, $rootScope, $http) {
+    //This controller method instantiation of holder user info i.e holder info for the CRUD operation. 
+    function holderCtrl($scope, $rootScope, $http, userAccountService) {
+        $scope.isReleased = false;
+        $scope.userProfile = userAccountService.userProfile;
+        if ($rootScope.userProfile === null || $rootScope.userProfile === 'undefined') {
+            $state.go('login');
+        }
+
+        // The below method will fetches the user data to populate on the views.
         $scope.getProfile = function () {
             $http({
-                method: "GET",
-                url: $rootScope.apiURL + "useraccount/GetUserProfile?userid=sai_patha@epam.com" //+ $scope.userName
-            }).then(function mySuccess(response) {
-                $scope.userProfile = response.data;
-            }, function myError(response) {
-                $scope.userProfile = "No data found for specified user";
+                method: 'GET',
+                url: $rootScope.apiURL + 'useraccount/GetUserProfile?userid=' + userAccountService.userProfile.userName
+            }).then(function (response) {
+                userAccountService.userProfile = response.data;
+            }, function () {
+                $scope.userProfile = 'No data found for specified user';
             });
-        }
-        var isReleased = false;
-        $scope.releaseSlot = function () {
+        };
 
+        //This function is relase the slot based on the request
+        $scope.releaseSlot = function () {
             $scope.isReleased = true;
-            $scope.successMessage = "Thank you!! your slot release successfully";
-        }
+            $scope.successMessage = 'Thank you!! your slot release successfully';
+        };
     }
 }());
