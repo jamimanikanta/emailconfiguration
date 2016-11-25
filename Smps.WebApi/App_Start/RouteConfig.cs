6cc2d34870150b92ae0a811a6f6809460c21aaed
pavankumargautaraju <pavan_gautaraju@epam.com>
2016-11-25 16:00:09 +0530

    Comitting the changes related to infrsstructure-SNLPROJECT-2094

--- Smps.WebApi/App_Start/RouteConfig.cs
+++ Smps.WebApi/App_Start/RouteConfig.cs
@@ -3,11 +3,6 @@
 //     Company copyright tag.
 // </copyright>
 //<summary>This is the configuration class.</summary>
-//Route defines the URL pattern and handler information.
-//All the configured routes of an application stored in RouteTable 
-//And will be used by Routing engine to determine appropriate handler class or file for an incoming request.
-//Every MVC application must configure (register) at least one route, 
-//Which is configured by MVC framework by default
 //-----------------------------------------------------------------------
 
 namespace Smps.WebApi
