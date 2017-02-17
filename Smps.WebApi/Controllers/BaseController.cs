6e7ad8a3fb3c748550df1df39ff51d7e0da4e86e
Venkatesh Pydi <Venkatesh_Pydi@epam.com>
2017-02-17 16:59:03 +0530

    Added Corss attribute for header.

--- Smps.WebApi/Controllers/BaseController.cs
+++ Smps.WebApi/Controllers/BaseController.cs
@@ -8,11 +8,13 @@
 namespace Smps.WebApi.Controllers
 {
     using System.Web.Http;
-
+    using System.Web.Http.Cors;
+    
     /// <summary>
     /// This class is the base controller for all web application controllers
     /// This will contain all the common methods related to all the controllers
     /// </summary>
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
     public class BaseController : ApiController
     {
     }
